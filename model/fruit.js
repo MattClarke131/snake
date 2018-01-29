a7c53c241200523ce3668dd21e9a54a5a098669d
matthew <mattclarkeruns@gmail.com>
2018-01-28 23:47:02 -0500

    Create fruit class

---
+++ model/fruit.js
@@ -0,0 +1,21 @@
+// fruit constructor
+// A fruit is an object with an array of coordinate pairs, a pointValue,
+// and a remainingLifeSpan
+class Fruit extends GameEntity {
+  constructor(positionArray, pointValue, remainingLifeSpan) {
+    super(positionArray);
+    if (typeof pointValue != number) {
+      throw new Error('pointValue must be a number');
+    } else if (typeof remainingLifeSpan != number) {
+      throw new Error('remainingLifeSpan must be a number');
+    }
+    this._pointvalue = pointValue;
+    this._remainingLifeSpan = remainingLifeSpan;
+  }
+  get pointvalue() {
+    return this._pointValue;
+  }
+  get remainingLifeSpan() {
+    return this._remainingLifeSpan;
+  }
+};
